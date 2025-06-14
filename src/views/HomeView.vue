6b7f3ac24700dd7b7291f1837fecb77a23310e32
Rinon Mac <rinon.mac@outlook.com>
2025-06-14 23:09:54 +0700

    Updated script

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import FormPokemon from '../components/FormPokemon.vue'
+
+// Provide router instance to be used by FormPokemon component
+const router = useRouter()
 </script>
 
 <template>
